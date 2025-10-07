742670e216a74bc2459a67342847c2e46336e696
vanessa ozogu <vanessaozogu@gmail.com>
2025-10-07 22:05:01 +0200

    added a JPQL query to the Booksrepository file

--- BooksJpa/src/main/java/com/ironhack/booksjpa/repository/BooksRepository.java
+++ BooksJpa/src/main/java/com/ironhack/booksjpa/repository/BooksRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -12,6 +13,12 @@ public interface BooksRepository extends JpaRepository<Books, Integer> {
 
     List<Books> findAll();
     List<Books> findAllById(Iterable<Integer> integers);
+    List<Books> findByAuthor(String author);                       // 1
+    List<Books> findByGenreAndPublisher(String genre, String publisher); // 2
+    List<Books> findByReleaseDateAfter(LocalDate date);            // 3
+
+    @Query("SELECT b FROM Books b WHERE b.title LIKE %:keyword%")
+    List<Books> findBooksByTitleContains(String keyword);
 
     @Query(value = "SELECT * FROM Books b ORDER BY b.releaseDate ASC", nativeQuery = true)
     List<Books> findAllByReleaseDateOrderByReleaseDateDesc();
